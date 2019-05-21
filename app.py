eecc9e157741a2ed8dbe59a78029345768e9f806
Chris Schnaufer <schnaufer@email.arizona.edu>
2019-05-21 14:06:04 -0700

    Test

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+import time
+
+ts = "App [" + time.ctime() + "] "
+
+argc = len(sys.argv)
+print(ts + "Have " + str(argc) + " command line arguments")
+if argc > 1:
+    for i in range(1, argc):
+        print(ts+"Arg "+str(i)+" '"+argv[i]+"'")
